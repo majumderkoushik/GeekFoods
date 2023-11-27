0b3230eae4b5bf97d4fac9a3646771c4ec86b94c
majumderkoushik <majumderkoushikme@gmail.com>
2023-11-27 19:49:50 +0530

    2nd commit

--- src/assets/Foods/RecipeIndex.jsx
+++ src/assets/Foods/RecipeIndex.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RecipeIndex = ({alphaIndex}) => {
     const alpha =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    var num=0;
+    let num=0;
   return (
     <>
     {
