adef8a69eb3deeebb679fc610b85ca1d93d95428
majumderkoushik <majumderkoushikme@gmail.com>
2023-11-30 02:10:37 +0530

    6th

--- src/assets/Foods/Foods.jsx
+++ src/assets/Foods/Foods.jsx
@@ -4,13 +4,15 @@ import React from 'react'
 import NavBar from '../NavBar/NavBar'
 import Meal from './Meal'
 import Footer from '../Footer/Footer'
+import MealItem from './MealItem'
 
 
 function Foods() {
   return (
     <>
     <div className=' bg-indigo-400 p-5'>
-    <Meal />     
+    <Meal />  
+    <MealItem />   
     </div>
     <Footer />
     
